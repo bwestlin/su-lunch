057517b61a1607d3ae57a71b2f391bcac12eb1e7
Björn Westlin <bwestlin@gmail.com>
2013-05-30 23:13:20 +0200

    Changed to using webjars for asset dependencys

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,11 @@ object ApplicationBuild extends Build {
     val appDependencies = Seq(
       // Add your project dependencies here,
       "org.jsoup" % "jsoup" % "1.7.1",
-      "joda-time" % "joda-time" % "2.1"
+      "joda-time" % "joda-time" % "2.1",
+      "org.webjars" %% "webjars-play" % "2.1.0-2",
+      "org.webjars" % "jquery" % "1.9.1",
+      "org.webjars" % "bootstrap" % "2.1.1",
+      "org.webjars" % "font-awesome" % "3.0.2"
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
